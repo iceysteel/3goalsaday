f2947bf11964f177216023b1c244b92f3df67091
Zain Munad <zainm211@hotmail.com>
2015-01-17 22:36:15 -0800

    saves goals to sql database now

--- setgoals.php
+++ setgoals.php
@@ -1,10 +1,37 @@
 <?php
 
+//this function takes a varible and feild and puts var into the mysql feild
+function putsql($var, $field)
+{
+    $sql = "INSERT INTO goals ($field)
+    VALUES ('".$var."')";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
+
+//code for connecting to mysql server
+$servername = "localhost";
+$username = "root";
+$password = "tDulVhXDuH";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+mysqli_select_db($conn,"3goals");
+
+//code to decide what gets put into the sql database
 if (isset($_POST['goal1submit']))
   {
   // Execute this code if the submit button is pressed.
-    $goal1 = $_POST['goal1form'];
-    $goal1info = $_POST['goal1infoform'];
+    putsql($_POST['goal1form'], 'goal1');
+    putsql($_POST['goal1infoform'], 'goal1info');
   }
 
 if (isset($_POST['goal2submit']))
