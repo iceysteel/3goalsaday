bd06fcf1c665f90acf47812b8eef4ed96b674b0c
Zain Munad <zainm211@hotmail.com>
2015-01-20 17:57:22 -0800

    first attempt at retriving data from sql

--- index.php
+++ index.php
@@ -70,7 +70,40 @@
             </div>
         </div>
         
-        
+<?php
+    
+    //code to connect to sql database and get goal data
+    $servername = "localhost";
+    $username = "root";
+    $password = "tDulVhXDuH";
+    $dbname = "3goals";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+/*    $sql = "SELECT id, firstname, lastname FROM goals";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }*/
+
+    
+    $sql = "SELECT goal1 FROM goals ORDER BY column_name DESC LIMIT 1;"
+    $result = $conn->query($sql);
+    $goal1 = $result;
+
+    $conn->close();
+?>
         
         
         <div class="row clearfix">
